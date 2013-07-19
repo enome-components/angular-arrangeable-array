931b7716abcd231f3c8bbaf46066837ab345e761
Geert Pasteels <geert.pasteels@gmail.com>
2013-07-19 11:50:08 +0200

    with dropareas

--- index.js
+++ index.js
@@ -35,7 +35,87 @@ module.directive('arrangeableArray', function () {
       };
 
       // Events
+      
+      var current;
+      var current_drop;
+      
+      $scope.dragStart = function (e) {
 
+        current = e;
+        current.offsetY = e.pageY - current.target.offsetTop; // FF doesn't support offsetY
+        
+        // Prevent selection
+        document.onmousedown = function () { return false; };
+
+        // On drop
+        document.onmouseup = function () {
+          if (current) {
+            current.target.style.zIndex = 0;
+            current.target.style.position = 'inherit';
+            current.target.style.top = 'inherit';
+            resetExpand();
+            (current = null);
+          }
+        };
+
+        document.onmousemove = function (e) {
+
+          if (current) {
+            current.target.style.position = 'absolute';
+            current.target.style.zIndex = 10;
+            current.target.style.top = (e.pageY - current.offsetY) + 'px';
+
+            var dropareas = root[0].querySelectorAll('.droparea');
+
+            [].forEach.call(dropareas, function (droparea) {
+
+              var cords = droparea.getBoundingClientRect();
+
+              if (e.pageX >= cords.left && e.pageX <= cords.right && e.pageY >= cords.top && e.pageY <= cords.bottom) {
+                resetExpand();
+                droparea.classList.add('expand');
+              }
+
+            });
+
+
+          }
+
+        };
+
+      };
+
+      /*
+      root.bind('mousemove', function (e) {
+
+        if (current) {
+          console.log(e.target, 'over');
+          //resetExpand();
+          //e.target.classList.add('expand');
+        }
+
+      });
+      */
+
+      /*
+      var drag_el;
+
+      $document.bind('mousemove', select('row', function (e) {
+
+        if (e.which === 1) {
+          e.target.style.background = 'red';
+          e.target.style.position = 'absolute';
+          e.target.style.top = (e.clientY) + 'px';
+          e.target.style.left = (e.clientX) + 'px';
+          e.stopPropagation();
+          e.preventDefault();
+          return false;
+        }
+
+      }));
+      */
+
+      /*
       root.bind('dragstart', select('row', function (e) {
         e.target.classList.add('dragging');
         e.dataTransfer.setData('text/plain', 'This text may be dragged');
@@ -57,7 +137,6 @@ module.directive('arrangeableArray', function () {
         e.target.classList.add('expand');
       }));
 
-      /*
       root.bind('dragleave', select('droparea', function (e) {
         e.target.classList.remove('expand');
       }));
--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '<div>\n  <div class=\'block\'></div>\n\n  <div ng-repeat=\'item in array\'>\n\n    <div class=\'droparea\'>\n\n    </div>\n\n    <div draggable=\'true\' class=\'row\'>\n      {{item}}\n      <button ng-click=\'remove(item)\'>trash</button>\n    </div>\n\n  </div>\n\n  <div class=\'droparea\'>\n\n  </div>\n\n</div>\n';
+module.exports = '<div>\n\n  <div ng-repeat=\'item in array\'>\n\n    <div class=\'row\' ng-mousedown=\'dragStart($event)\'>\n      {{item}}\n      <button ng-click=\'remove(item)\'>trash</button>\n    </div>\n\n  </div>\n\n</div>\n';
